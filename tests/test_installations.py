586512c627b8b81f2d104daaf7e0c5c6ed955b08
Abhi Ojha <abhiojha8@gmail.com>
2023-04-06 23:15:46 +0530

    update tests

--- tests/test_installations.py
+++ tests/test_installations.py
@@ -1,39 +1,63 @@
 import pytest
-import torch
 import numpy as np
 import pandas as pd
-import matplotlib
-import sklearn
-import scipy
+import torch
+from torchtext.datasets import AG_NEWS
+from sklearn.linear_model import LinearRegression
+from transformers import pipeline
+import nltk
+nltk.download('punkt')
 
 
-class TestPyTorchInstallation:
-    def test_pytorch_version(self):
-        assert torch.__version__ == "1.13.1", "PyTorch version is not 1.13.1"
+@pytest.fixture
+def test_data():
+    # Generate some test data
+    inputs = ["This is a test sentence.", "Another sentence to test with."]
+    X = np.random.rand(100, 3)
+    y = 2*X[:,0] + 3*X[:,1] + 4*X[:,2] + np.random.randn(100)
+    df = pd.DataFrame(X, columns=['x1', 'x2', 'x3'])
+    return inputs, df, y
 
-    def test_cuda_available(self):
-        assert torch.cuda.is_available(), "CUDA is not available"
+def test_linear_regression(test_data):
+    # Fit a linear regression model to the test data
+    inputs, X, y = test_data
+    model = LinearRegression().fit(X, y)
 
-    def test_tensor_addition(self):
-        x = torch.tensor([1, 2, 3])
-        y = torch.tensor([4, 5, 6])
-        z = x + y
-        assert z.tolist() == [5, 7, 9], "Tensor addition is incorrect"
+    # Predict on some new data
+    new_X = np.random.rand(10, 3)
+    new_df = pd.DataFrame(new_X, columns=['x1', 'x2', 'x3'])
+    preds = model.predict(new_X)
+
+    # Check that the predictions are reasonable
+    assert np.allclose(preds, model.predict(new_df))
+
+def test_torch():
+    # Test basic tensor operations
+    a = torch.tensor([1, 2, 3])
+    b = torch.tensor([4, 5, 6])
+    c = a + b
+    assert torch.equal(c, torch.tensor([5, 7, 9]))
 
 
-class TestLibrariesInstallation:
-    def test_numpy_version(self):
-        assert np.__version__ == "1.23.5", "NumPy version is not 1.21.3"
+def test_transformers():
+    # Test using a pre-trained model from transformers
+    generator = pipeline("text-generation", model="gpt2")
 
-    def test_pandas_version(self):
-        assert pd.__version__ == "1.5.3", "Pandas version is not 1.3.5"
+    # Generate some text using the model
+    text = generator("This is a test", max_length=20, do_sample=False)[0]['generated_text']
 
-    def test_matplotlib_version(self):
-        assert matplotlib.__version__ == "3.7.0", "Matplotlib version is not 3.5.1"
+    # Check that the generated text is non-empty
+    assert len(text) > 0
 
-    def test_sklearn_version(self):
-        assert sklearn.__version__ == "1.2.1", "Scikit-learn version is not 1.0.1"
+def test_nltk_tokenization():
+    # Define a simple sentence to tokenize
+    sentence = "This is a simple sentence."
 
-    def test_scipy_version(self):
-        assert scipy.__version__ == "1.10.0", "SciPy version is not 1.7.3"
+    # Test that word tokenization works correctly
+    expected_words = ["This", "is", "a", "simple", "sentence", "."]
+    assert nltk.word_tokenize(sentence) == expected_words
+
+    # Test that sentence tokenization works correctly
+    expected_sentences = ["This is a simple sentence."]
+    assert nltk.sent_tokenize(sentence) == expected_sentences
 
